1f98531c782ed0a32ae35cb8b7ae047223a8810f
Joy Johns <JRJ20716@gmail.com>
2023-04-30 23:33:53 -0400

    Created queries.js & mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,72 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeFriend($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+
+
+LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+
+ADD_USER will execute the addUser mutation.
+
+SAVE_BOOK will execute the saveBook mutation.
+
+REMOVE_BOOK will execute the removeBook mutation.
